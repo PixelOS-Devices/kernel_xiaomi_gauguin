2864ab26bf198b2bc1820990ac9caec70da37ad3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-31 20:52:59 +0530

    msm: dsi-staging: Import Xiaomi changes
    - This change adds support for tddi_doubleclick_flag,
      which is responsible for support of DT2W.
    
    Co-authored-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Change-Id: Id085dfd6d32e0b1514c73aacf9528547208c5999
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -14,6 +14,7 @@
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
 #include "dsi_parser.h"
+#include "dsi_display.h"
 #include "sde_dbg.h"
 
 /**
@@ -452,6 +453,7 @@ static int dsi_panel_power_on(struct dsi_panel *panel)
 {
 	int rc = 0;
 
+	if (!panel->tddi_doubleclick_flag)
 	rc = dsi_pwr_enable_regulator(&panel->power_info, true);
 	if (rc) {
 		DSI_ERR("[%s] failed to enable vregs, rc=%d\n",
@@ -496,6 +498,7 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	if (gpio_is_valid(panel->reset_config.disp_en_gpio))
 		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
+	if (!panel->tddi_doubleclick_flag)
 	if (gpio_is_valid(panel->reset_config.reset_gpio) &&
 					!panel->reset_gpio_always_on)
 		gpio_set_value(panel->reset_config.reset_gpio, 0);
@@ -516,6 +519,7 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 		       rc);
 	}
 
+	if(!panel->tddi_doubleclick_flag)
 	rc = dsi_pwr_enable_regulator(&panel->power_info, false);
 	if (rc)
 		DSI_ERR("[%s] failed to enable vregs, rc=%d\n",
@@ -3470,6 +3474,7 @@ struct dsi_panel *dsi_panel_get(struct device *parent,
 	if (rc)
 		DSI_DEBUG("failed to parse esd config, rc=%d\n", rc);
 
+	panel->tddi_doubleclick_flag = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
 	drm_panel_init(&panel->drm_panel);
 	panel->drm_panel.dev = &panel->mipi_device.dev;
@@ -4605,3 +4610,10 @@ error:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
 }
+
+void dsi_panel_doubleclick_enable(bool on) {
+	struct dsi_display *primary_display = get_main_display();
+	if (primary_display && primary_display->panel)
+		primary_display->panel->tddi_doubleclick_flag = on;
+}
+EXPORT_SYMBOL(dsi_panel_doubleclick_enable);
