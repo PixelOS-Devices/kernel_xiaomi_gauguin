a09a301a5b2f3d2ab2dacdb3417833e134eda2af
kawaaii <kawaaii@nocturn9x.space>
2023-06-04 09:22:40 +0545

    techpack: audio: Opt out DEBUG
    grep -rlZ "#define DEBUG" techpack/audio | xargs -0 sed -i '/#define DEBUG/d'
    
    Change-Id: If865975bfa072e4e94efb4658ef85860e99f433a

--- techpack/audio/asoc/kona.c
+++ techpack/audio/asoc/kona.c
@@ -3,7 +3,6 @@
  * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  * Copyright (C) 2021 XiaoMi, Inc.
  */
-#define DEBUG
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
