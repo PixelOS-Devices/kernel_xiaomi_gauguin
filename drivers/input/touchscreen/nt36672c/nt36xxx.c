10bdfb012fbd59dc55e5c9c9240decdaea5456d2
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-08-08 01:16:38 +0545

    nt36672c: Setup IRQ properly during nvt_ts_resume()
    As seen on upstream NVT NT36xxx SPI project.
    
    Change-Id: I3c2668c1a7972459041529e7cb4ed28835229110

--- drivers/input/touchscreen/nt36672c/nt36xxx.c
+++ drivers/input/touchscreen/nt36672c/nt36xxx.c
@@ -2384,8 +2384,9 @@ static int32_t nvt_ts_resume(struct device *dev)
 		NVT_ERR("download firmware failed\n");
 	nvt_check_fw_reset_state(RESET_STATE_REK);
 
-
-	nvt_irq_enable(true);
+	if (!ts->db_wakeup) {
+		nvt_irq_enable(true);
+	}
 
 #if NVT_TOUCH_ESD_PROTECT
 	nvt_esd_check_enable(false);
