85e2318354b3a123209b69ec7b647c6a38daf000
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-08-27 01:27:53 +0545

    techpack: camera: Import Xiaomi Changes
    From gauguin-r-oss
    
    Change-Id: I5c46eef57ea63c137a13923d0c037924ea37503f
    Signed-off-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- techpack/camera/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
+++ techpack/camera/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <linux/slab.h>
@@ -7350,7 +7351,8 @@ static void cam_ife_mgr_ctx_irq_dump(struct cam_ife_hw_mgr_ctx *ctx)
 
 static int cam_ife_hw_mgr_check_irq_for_dual_vfe(
 	struct cam_ife_hw_mgr_ctx            *ife_hw_mgr_ctx,
-	uint32_t                              hw_event_type)
+	uint32_t                              hw_event_type,
+	bool                                  detect_mismatch)
 {
 	int32_t                               rc = -1;
 	uint32_t                             *event_cnt = NULL;
@@ -7386,12 +7388,12 @@ static int cam_ife_hw_mgr_check_irq_for_dual_vfe(
 		return rc;
 	}
 
-	if ((event_cnt[master_hw_idx] &&
+	if (detect_mismatch && ((event_cnt[master_hw_idx] &&
 		((int)(event_cnt[master_hw_idx] - event_cnt[slave_hw_idx]) > 1
 		)) ||
 		(event_cnt[slave_hw_idx] &&
 		((int)(event_cnt[slave_hw_idx] - event_cnt[master_hw_idx]) > 1
-		))) {
+		)))) {
 
 		CAM_ERR_RATE_LIMIT(CAM_ISP,
 			"One of the VFE could not generate hw event %d master[%d] core_cnt %d slave[%d] core_cnt %d",
@@ -7449,7 +7451,11 @@ static int cam_ife_hw_mgr_handle_hw_epoch(
 	case CAM_ISP_HW_VFE_IN_CAMIF:
 		ife_hw_mgr_ctx->epoch_cnt[event_info->hw_idx]++;
 		rc = cam_ife_hw_mgr_check_irq_for_dual_vfe(ife_hw_mgr_ctx,
-			CAM_ISP_HW_EVENT_EPOCH);
+			CAM_ISP_HW_EVENT_SOF, false);
+		if (rc)
+			break;
+		rc = cam_ife_hw_mgr_check_irq_for_dual_vfe(ife_hw_mgr_ctx,
+			CAM_ISP_HW_EVENT_EPOCH, true);
 		if (!rc) {
 			epoch_done_event_data.frame_id_meta =
 				event_info->reg_val;
@@ -7506,7 +7512,7 @@ static int cam_ife_hw_mgr_handle_hw_sof(
 	case CAM_ISP_HW_VFE_IN_RD:
 		ife_hw_mgr_ctx->sof_cnt[event_info->hw_idx]++;
 		rc = cam_ife_hw_mgr_check_irq_for_dual_vfe(ife_hw_mgr_ctx,
-			CAM_ISP_HW_EVENT_SOF);
+			CAM_ISP_HW_EVENT_SOF, true);
 		if (!rc) {
 			if (ife_hw_mgr_ctx->is_offline)
 				cam_ife_hw_mgr_get_offline_sof_timestamp(
@@ -7581,7 +7587,7 @@ static int cam_ife_hw_mgr_handle_hw_eof(
 	case CAM_ISP_HW_VFE_IN_CAMIF:
 		ife_hw_mgr_ctx->eof_cnt[event_info->hw_idx]++;
 		rc = cam_ife_hw_mgr_check_irq_for_dual_vfe(ife_hw_mgr_ctx,
-			CAM_ISP_HW_EVENT_EOF);
+			CAM_ISP_HW_EVENT_EOF, true);
 		if (!rc) {
 			ife_hw_irq_eof_cb(ife_hw_mgr_ctx->common.cb_priv,
 				CAM_ISP_HW_EVENT_EOF, &eof_done_event_data);
